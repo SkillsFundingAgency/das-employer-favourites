03b005b1b2e173007f1d35ee909f71a52c1c0c92
Lee Wadhams <lee.wadhams@valtech.co.uk>
2019-03-04 16:07:06 +0000

    Add skeleton projects

---
+++ src/web/SavedSelectionsWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SavedSelectionsWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
