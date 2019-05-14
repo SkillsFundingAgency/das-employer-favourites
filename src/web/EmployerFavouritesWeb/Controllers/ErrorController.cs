460a5c328af9d2ee66be5b5f5637cb8ec435c457
Lee Wadhams <lee.wadhams@valtech.co.uk>
2019-05-14 15:53:50 +0100

    Update exception handling

--- src/web/EmployerFavouritesWeb/Controllers/ErrorController.cs
+++ src/web/EmployerFavouritesWeb/Controllers/ErrorController.cs
@@ -1,12 +1,23 @@
+using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Net;
 using DfE.EmployerFavourites.Web.Models;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace DfE.EmployerFavourites.Web.Controllers
 {
     public class ErrorController : Controller
-    {        
+    {
+        private readonly ILogger<ErrorController> _logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
+        
         [Route("error/{id?}")]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error(int id)
@@ -18,6 +29,24 @@ namespace DfE.EmployerFavourites.Web.Controllers
 
         private string GetViewNameForStatus(int statusCode)
         {
+            var exceptionFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            if (exceptionFeature != null)
+            {
+                string routeWhereExceptionOccurred = exceptionFeature.Path;
+                var exception = exceptionFeature.Error;
+
+                if (exception is AggregateException aggregateException)
+                {
+                    var flattenedExceptions = aggregateException.Flatten();
+                    _logger.LogError(flattenedExceptions, "Aggregate exception on path: {route}", routeWhereExceptionOccurred);
+
+                    exception = flattenedExceptions.InnerExceptions.FirstOrDefault();
+                }
+
+                _logger.LogError(exception, "Unhandled exception on path: {route}", routeWhereExceptionOccurred);
+            }
+
             switch(statusCode)
             {
                 case (int)HttpStatusCode.NotFound:
