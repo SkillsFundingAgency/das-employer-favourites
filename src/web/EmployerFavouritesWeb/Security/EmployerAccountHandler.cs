351b62a006802ba66d3bd615b3ee7ded27c7e775
Matt Derry <matt.derry@digital.education.gov.uk>
2019-05-14 12:59:10 +0100

    Adds redirect for users without at least one employer account (#21)
    * Adds redirect for users without at least one employer account
    
    * Fixes test for employer account handler
    
    * signs out user from app when redirecting to employer registration

--- src/web/EmployerFavouritesWeb/Security/EmployerAccountHandler.cs
+++ src/web/EmployerFavouritesWeb/Security/EmployerAccountHandler.cs
@@ -1,13 +1,23 @@
 using System.Linq;
 using System.Threading.Tasks;
+using DfE.EmployerFavourites.Web.Configuration;
 using EmployerFavouritesWeb.Security;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Options;
 
 namespace DfE.EmployerFavourites.Web.Security
 {
     public class EmployerAccountHandler : AuthorizationHandler<EmployerAccountRequirement>
     {
+        private IOptions<ExternalLinks> _externalLinks;
+        public EmployerAccountHandler(IOptions<ExternalLinks> externalLinks)
+        {
+            _externalLinks = externalLinks;
+        }
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, EmployerAccountRequirement requirement)
         {
             if (context.Resource is AuthorizationFilterContext mvcContext && mvcContext.RouteData.Values.ContainsKey("employerAccountId"))
@@ -15,7 +25,16 @@ namespace DfE.EmployerFavourites.Web.Security
                 if (context.User.HasClaim(c => c.Type.Equals(EmployerClaims.AccountsClaimsTypeIdentifier)))
                 {
                     var accountIdFromUrl = mvcContext.RouteData.Values["employerAccountId"].ToString().ToUpper();
-                    var employerAccounts = context.User.GetEmployerAccounts();
+                    var employerAccounts = context.User.GetEmployerAccounts().ToList();
+
+                    if (employerAccounts.Any() == false)
+                    {
+                        mvcContext.Result = new RedirectResult($"{_externalLinks.Value.AccountsRegistrationPage}?returnUrl={mvcContext.HttpContext.Request.GetEncodedUrl()}");
+                        mvcContext.HttpContext.SignOutAsync("Cookies");
+
+                        //Although not a 'success', the redirect only works if calling Succeed(), please see: https://stackoverflow.com/questions/41707051
+                        context.Succeed(requirement);
+                    }
 
                     if (employerAccounts.Contains(accountIdFromUrl))
                     {
--- src/web/UnitTests/Security/EmployerAccountHandlerTests.cs
+++ src/web/UnitTests/Security/EmployerAccountHandlerTests.cs
@@ -1,42 +1,165 @@
+using System;
 using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using DfE.EmployerFavourites.Web.Configuration;
 using DfE.EmployerFavourites.Web.Security;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Options;
+using Moq;
 using Xunit;
 
 namespace DfE.EmployerFavourites.UnitTests.Security
 {
     public class EmployerAccountHandlerTests
     {
+        private Mock<IOptions<ExternalLinks>> _mockExternalLinks;
+        private Uri _testRegistrationLink = new Uri("http://test.com");
 
         public EmployerAccountHandlerTests()
         {
-
+            _mockExternalLinks = new Mock<IOptions<ExternalLinks>>();
+            _mockExternalLinks.Setup(s => s.Value).Returns(new ExternalLinks()
+                {AccountsRegistrationPage = _testRegistrationLink});
         }
 
         [Fact]
-        public async Task HandleRequirementAsync_UserDoesNotHaveMatchingAccountId_Fails()
+        public async Task HandleRequirementAsync_UserDoesNotHaveMatchingAccountId_Success()
         {
+            var authServiceMock = new Mock<IAuthenticationService>();
+            authServiceMock
+                .Setup(_ => _.SignOutAsync(It.IsAny<HttpContext>(), It.IsAny<string>(), It.IsAny<AuthenticationProperties>()))
+                .Returns(Task.FromResult((object)null));
+
+            var serviceProviderMock = new Mock<IServiceProvider>();
+            serviceProviderMock
+                .Setup(_ => _.GetService(typeof(IAuthenticationService)))
+                .Returns(authServiceMock.Object);
+
             var requirements = new [] { new EmployerAccountRequirement()};
             var user = new ClaimsPrincipal(
                         new ClaimsIdentity(
                             new Claim[] {
-                                new Claim(EmployerClaims.AccountsClaimsTypeIdentifier, "['XYZ123']")
+                                new Claim(EmployerClaims.AccountsClaimsTypeIdentifier, "")
                             },
                             "Basic")
                         );
                         
             var routeData = new RouteData(new RouteValueDictionary(new Dictionary<string, string> { {"employerAccountId", "ABC123"} }));
             var actionContext = new ActionContext(new DefaultHttpContext(), routeData, new ControllerActionDescriptor());
+            var resource = new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>()) { HttpContext = new DefaultHttpContext() { RequestServices = serviceProviderMock.Object } };
+            var context = new AuthorizationHandlerContext(requirements, user, resource);
+            var subject = new EmployerAccountHandler(_mockExternalLinks.Object);
+
+            await subject.HandleAsync(context);
+
+            Assert.True(context.HasSucceeded);
+        }
+
+        [Fact]
+        public async Task HandleRequirementAsync_UserDoesNotHaveMatchingAccountId_RedirectsToRegistration()
+        {
+            var authServiceMock = new Mock<IAuthenticationService>();
+            authServiceMock
+                .Setup(_ => _.SignOutAsync(It.IsAny<HttpContext>(), It.IsAny<string>(), It.IsAny<AuthenticationProperties>()))
+                .Returns(Task.FromResult((object)null));
+
+            var serviceProviderMock = new Mock<IServiceProvider>();
+            serviceProviderMock
+                .Setup(_ => _.GetService(typeof(IAuthenticationService)))
+                .Returns(authServiceMock.Object);
+
+            var requirements = new[] { new EmployerAccountRequirement() };
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(EmployerClaims.AccountsClaimsTypeIdentifier, "")
+                    },
+                    "Basic")
+            );
+
+            var routeData = new RouteData(new RouteValueDictionary(new Dictionary<string, string> { { "employerAccountId", "ABC123" } }));
+            var actionContext = new ActionContext(new DefaultHttpContext(), routeData, new ControllerActionDescriptor());
+            var resource = new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>()){HttpContext = new DefaultHttpContext(){RequestServices = serviceProviderMock .Object} };
+            var context = new AuthorizationHandlerContext(requirements, user, resource);
+            var subject = new EmployerAccountHandler(_mockExternalLinks.Object);
+
+            await subject.HandleAsync(context);
+
+            Assert.IsType<AuthorizationFilterContext>(context.Resource);
+            var result = context.Resource as AuthorizationFilterContext;
+
+            Assert.IsType<RedirectResult>(result.Result);
+            var redirectResult = result.Result as RedirectResult;
+
+            Assert.Equal(_testRegistrationLink.AbsoluteUri + "?returnUrl=:///",redirectResult.Url);
+        }
+
+        [Fact]
+        public async Task HandleRequirementAsync_UserDoesNotHaveMatchingAccountId_RemovesAuthCookie()
+        {
+            var authServiceMock = new Mock<IAuthenticationService>();
+            authServiceMock
+                .Setup(_ => _.SignOutAsync(It.IsAny<HttpContext>(), It.IsAny<string>(), It.IsAny<AuthenticationProperties>()))
+                .Returns(Task.FromResult((object)null));
+
+            var serviceProviderMock = new Mock<IServiceProvider>();
+            serviceProviderMock
+                .Setup(_ => _.GetService(typeof(IAuthenticationService)))
+                .Returns(authServiceMock.Object);
+
+            var requirements = new[] { new EmployerAccountRequirement() };
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(EmployerClaims.AccountsClaimsTypeIdentifier, "")
+                    },
+                    "Basic")
+            );
+
+            var routeData = new RouteData(new RouteValueDictionary(new Dictionary<string, string> { { "employerAccountId", "ABC123" } }));
+            var actionContext = new ActionContext(new DefaultHttpContext(), routeData, new ControllerActionDescriptor());
+            var resource = new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>()) { HttpContext = new DefaultHttpContext() { RequestServices = serviceProviderMock.Object} };
+            var context = new AuthorizationHandlerContext(requirements, user, resource);
+            var subject = new EmployerAccountHandler(_mockExternalLinks.Object);
+
+            await subject.HandleAsync(context);
+
+            Assert.IsType<AuthorizationFilterContext>(context.Resource);
+            var result = context.Resource as AuthorizationFilterContext;
+
+            Assert.IsType<RedirectResult>(result.Result);
+            var redirectResult = result.Result as RedirectResult;
+
+            Assert.Equal(_testRegistrationLink.AbsoluteUri + "?returnUrl=:///", redirectResult.Url);
+
+            authServiceMock.Verify(v => v.SignOutAsync(It.IsAny<HttpContext>(), "Cookies", It.IsAny<AuthenticationProperties>()),Times.Once);
+        }
+
+        [Fact]
+        public async Task HandleRequirementAsync_UserDoesNotHaveMatchingAccountId_Fail()
+        {
+            var requirements = new[] { new EmployerAccountRequirement() };
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(EmployerClaims.AccountsClaimsTypeIdentifier, "['XYZ123']")
+                    },
+                    "Basic")
+            );
+
+            var routeData = new RouteData(new RouteValueDictionary(new Dictionary<string, string> { { "employerAccountId", "ABC123" } }));
+            var actionContext = new ActionContext(new DefaultHttpContext(), routeData, new ControllerActionDescriptor());
             var resource = new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>());
             var context = new AuthorizationHandlerContext(requirements, user, resource);
-            var subject = new EmployerAccountHandler();
+            var subject = new EmployerAccountHandler(_mockExternalLinks.Object);
 
             await subject.HandleAsync(context);
 
@@ -59,7 +182,7 @@ namespace DfE.EmployerFavourites.UnitTests.Security
             var actionContext = new ActionContext(new DefaultHttpContext(), routeData, new ControllerActionDescriptor());
             var resource = new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>());
             var context = new AuthorizationHandlerContext(requirements, user, resource);
-            var subject = new EmployerAccountHandler();
+            var subject = new EmployerAccountHandler(_mockExternalLinks.Object);
 
             await subject.HandleAsync(context);
 
@@ -82,7 +205,9 @@ namespace DfE.EmployerFavourites.UnitTests.Security
             var actionContext = new ActionContext(new DefaultHttpContext(), routeData, new ControllerActionDescriptor());
             var resource = new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>());
             var context = new AuthorizationHandlerContext(requirements, user, resource);
-            var subject = new EmployerAccountHandler();
+            var subject = new EmployerAccountHandler(_mockExternalLinks.Object);
+
+          
 
             await subject.HandleAsync(context);
 
