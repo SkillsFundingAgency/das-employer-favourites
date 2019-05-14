e2b0859234b178a7f4fcf705c1705e9e74a4604d
Lee Wadhams <lee.wadhams@valtech.co.uk>
2019-05-14 17:18:14 +0100

    Update exception handling (#23)

--- src/web/EmployerFavouritesWeb/Controllers/ApprenticeshipsController.cs
+++ src/web/EmployerFavouritesWeb/Controllers/ApprenticeshipsController.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using DfE.EmployerFavourites.Application.Commands;
--- src/web/EmployerFavouritesWeb/Startup.Configure.cs
+++ src/web/EmployerFavouritesWeb/Startup.Configure.cs
@@ -31,7 +31,7 @@ namespace DfE.EmployerFavourites.Web
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
